e6e101a5c98d39b7d5ea5e99fb51457b017f0641
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-06-23 15:32:08 +0900

    update

--- src/modules/InitialMetaData.ts
+++ src/modules/InitialMetaData.ts
@@ -1,7 +1,7 @@
 export default {
     twCardType: "summary",
     twSite: "@Siro_yaka",
-    ogUrl: "",
+    ogUrl: "https://dotschedule-lite.siroyaka.net",
     ogTitle: "どっとスケジュール Lite",
     ogDescription: "どっとライブメンバーのYoutube配信スケジュールをまとめたサイトです。",
     ogImage: ""
