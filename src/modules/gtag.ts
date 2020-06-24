ea0eef4e89877d6b363980e10b35ab0209608183
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-06-24 16:01:24 +0900

    googleアナリティクス導入

---
+++ src/modules/gtag.ts
@@ -0,0 +1,20 @@
+export const GA_TRACKING_ID = 'UA-170493201-1';
+
+interface Window {
+  gtag: any
+}
+declare var window: Window;
+
+export const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url
+  });
+}
+
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+}
