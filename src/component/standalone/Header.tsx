f60690217cb7bf7a181717479520af86bb630fdd
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-06-30 14:31:38 +0900

    リロードボタンの追加 +1
    ページリロードボタンの追加
    クリックできるアイテムをクリックしたときの挙動を追加

--- src/component/standalone/Header.tsx
+++ src/component/standalone/Header.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import clsx from 'clsx';
 
+import { useRouter } from 'next/router';
+
 import { MenuButton } from 'component/template/IconButton';
+import { ReloadIconSvg } from 'component/parts/svgIcons';
 
 interface OwnProps {
   title: string,
@@ -12,11 +15,18 @@ type Props = OwnProps;
 
 const Header: React.FC<Props> = (props) => {
   const { title, onMenuClick } = props;
+  const router = useRouter();
+  const reload = React.useCallback(() => {
+    router.reload();
+  }, [router])
 
   return(
-    <header className={clsx('w-screen', 'flex', 'fixed', 'top-0', 'h-12', 'bg-orange-300', 'border', 'border-orange-300', 'px-4', 'py-1', 'flex', 'flex-row', 'items-center', 'z-50')}>
-      <MenuButton classes={{button: 'bg-orange-300 hover:bg-orange-200'}} onClick={onMenuClick} />
-      <h6 className={'font-sans font-semibold text-xl text-orange-600 mx-1 '}>{title}</h6>
+    <header className={clsx('w-screen', 'flex', 'fixed', 'top-0', 'h-12', 'bg-orange-300', 'border', 'border-orange-300', 'px-4', 'py-1', 'flex', 'flex-row', 'items-center', 'z-50', 'justify-between')}>
+      <div className={'flex flex-row items-center'}>
+        <MenuButton classes={{button: 'bg-orange-300 hover:bg-orange-200 active:bg-orange-100'}} onClick={onMenuClick} />
+        <h6 className={'font-sans font-semibold text-xl text-orange-600'}>{title}</h6>
+      </div>
+      <button className={'px-2 py-2 focus:outline-none hover:bg-orange-200 active:bg-orange-100 rounded-full'} style={{ transition: "all .15s ease" }} onClick={reload}><ReloadIconSvg /></button>
     </header>
   )
 }
