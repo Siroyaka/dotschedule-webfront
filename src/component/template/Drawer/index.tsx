bd1f7bb40068b8c2af71645a9aa31478607651e6
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-09-25 14:11:15 +0900

    ドロワーアニメーション追加

--- src/component/template/Drawer/index.tsx
+++ src/component/template/Drawer/index.tsx
@@ -13,21 +13,21 @@ type Props = OwnProps;
 
 const Drawer: React.FC<Props> = (props) => {
   const { className, children, open, onClose } = props;
-  const width = open ? 'w-64' : 'w-0';
-  const closeField = open ? 'w-full' : 'w-0';
-  if(!open) return null;
 
   return(
     <React.Fragment>
-      <article id='drawer-out-field' onClick={onClose} className={clsx('h-full', 'absolute', 'z-50', 'top-0', 'bg-gray-700', 'bg-opacity-25', closeField)} />
-      <article className={clsx(className, 'duration-500', 'ease-in-out', 'bg-red-300', 'h-full', 'absolute', 'z-50', 'top-0', 'transition', 'flex', 'flex-col', width)}>
-        <article id='drawerHeader' className={clsx('h-12', 'bg-orange-300', 'w-full', 'flex', 'flex-row', 'items-center', 'py-1', 'px-4')}>
+      {
+        open &&
+        <div id='drawer-out-field' onClick={onClose} className={clsx('h-full', 'absolute', 'z-50', 'top-0', 'bg-gray-700', 'bg-opacity-25', 'w-full')} />
+      }
+      <aside className={clsx(className, 'transform', 'duration-300', 'ease-in-out', 'bg-red-300', 'h-full', 'fixed', 'z-50', 'top-0', 'left-0', 'transition-all', 'flex', 'flex-col', 'w-64', `${open ? 'translate-x-0' : '-translate-x-full'}`)}>
+        <div id='drawerHeader' className={clsx('h-12', 'bg-orange-300', 'w-full', 'flex', 'flex-row', 'items-center', 'py-1', 'px-4')}>
           <button aria-label='close-left-drawer' className={clsx('py-2 px-2 focus:outline-none transition duration-200 rounded-full', 'hover:bg-orange-200', 'active:bg-orange-100')} style={{transition: 'all .15 ease'}} onClick={onClose}>
             <CloseSvg />
           </button>
-        </article>
+        </div>
         {children}
-      </article>
+      </aside>
     </React.Fragment>
   )
 }
