bfe0a84e3da0375d1510dc5a64e544984cdec708
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-10-04 20:22:16 +0900

    小さい画面の対応

--- src/pages/notice.tsx
+++ src/pages/notice.tsx
@@ -19,16 +19,16 @@ const NoticePage: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={'overflow-y-auto h-full'}>
-        <article className='max-w-4xl mx-auto my-16'>
-          <h1 className='text-4xl text-center w-full mb-16'>このサイトについて</h1>
+        <article className='max-w-4xl mx-8 lg:mx-auto my-8 sm:my-16'>
+          <h1 className='text-2xl sm:text-4xl text-center w-full mb-8 sm:mb-16'>このサイトについて</h1>
           <ul className='shadow-md border-2 px-8 py-8'>
             {notice.map(({title, text}, i) => (
               <li key={`notice-${i}`} className={'pb-4 mt-2'}>
-                <h1 className='text-2xl'>{title}</h1>
+                <h1 className='text-lg sm:text-2xl'>{title}</h1>
                 <ul className={'px-2 pt-1'}>
                   {text.map((r, xi) => (
                     <li key={`notice-${i}-row-${xi}`}>
-                      <span>{r}</span>
+                      <span className='text-sm sm:text-base'>{r}</span>
                     </li>
                   ))}
                 </ul>
