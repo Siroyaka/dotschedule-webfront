c949f78accf98d2364a828d8b5b9f617f9aa7922
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-11-24 20:32:08 +0900

    feat: 🎸 tailwindcss update support
    postcss and purgecss update for tailwindcss. And all
    orange-color-css-parts changed to yellow.
    
    BREAKING CHANGE: 🧨 orange to yellow

--- src/components/template/Drawer/index.tsx
+++ src/components/template/Drawer/index.tsx
@@ -21,8 +21,8 @@ const Drawer: React.FC<Props> = (props) => {
         <div id='drawer-out-field' onClick={onClose} className={clsx('h-full', 'absolute', 'z-50', 'top-0', 'bg-gray-700', 'bg-opacity-25', 'w-full')} />
       }
       <aside className={clsx(className, 'transform', 'duration-300', 'ease-in-out', 'bg-red-300', 'h-full', 'fixed', 'z-50', 'top-0', 'left-0', 'transition-all', 'flex', 'flex-col', 'w-64', `${open ? 'translate-x-0' : '-translate-x-full'}`)}>
-        <div id='drawerHeader' className={clsx('h-12', 'bg-orange-300', 'w-full', 'flex', 'flex-row', 'items-center', 'py-1', 'px-4')}>
-          <button aria-label='close-left-drawer' className={clsx('py-2 px-2 focus:outline-none transition duration-200 rounded-full', 'hover:bg-orange-200', 'active:bg-orange-100')} style={{transition: 'all .15 ease'}} onClick={onClose}>
+        <div id='drawerHeader' className={clsx('h-12', 'bg-yellow-300', 'w-full', 'flex', 'flex-row', 'items-center', 'py-1', 'px-4')}>
+          <button aria-label='close-left-drawer' className={clsx('py-2 px-2 focus:outline-none transition duration-200 rounded-full', 'hover:bg-yellow-200', 'active:bg-yellow-100')} style={{transition: 'all .15 ease'}} onClick={onClose}>
             <CloseSvg />
           </button>
         </div>
