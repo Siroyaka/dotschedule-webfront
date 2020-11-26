0a9cb1113442ea1f2a01c7185e68f0806f046d5e
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-11-26 09:50:18 +0900

    style: 💄 drawer menu padding x align

--- src/components/template/Drawer/index.tsx
+++ src/components/template/Drawer/index.tsx
@@ -20,8 +20,8 @@ const Drawer: React.FC<Props> = (props) => {
         open &&
         <div id='drawer-out-field' onClick={onClose} className='h-full absolute z-50 top-0 bg-gray-300 bg-opacity-25 w-full' />
       }
-      <aside className={clsx(className, 'bg-white transform duration-300 ease-in-out h-full fixed z-50 top-0 left-0 transition-all flex flex-col w-64 border-r border-gray-200', `${open ? 'translate-x-0' : '-translate-x-full'}`)}>
-        <div id='drawerHeader' className='h-12 bg-white border-b w-full flex flex-row items-center py-1 px-4'>
+      <aside className={clsx(className, 'bg-white transform duration-300 ease-in-out h-full fixed z-50 top-0 left-0 transition-all flex flex-col w-64 border-r border-gray-200 px-2', `${open ? 'translate-x-0' : '-translate-x-full'}`)}>
+        <div id='drawerHeader' className='h-12 bg-white border-b w-full flex flex-row items-center py-1'>
           <button aria-label='close-left-drawer' className={clsx('py-2 px-2 focus:outline-none transition duration-200 rounded-full', 'hover:bg-blue-100', 'active:bg-blue-200')} style={{transition: 'all .15 ease'}} onClick={onClose}>
             <CloseSvg />
           </button>
