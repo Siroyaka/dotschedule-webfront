18c6bc53a8bfe5afaecb88aeefd7a0d3d38ea9f0
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-11-26 11:10:10 +0900

    style: 💄 newspage always visible scroll bar

--- src/components/page/NewsPage.tsx
+++ src/components/page/NewsPage.tsx
@@ -25,9 +25,9 @@ const NewsPage: React.FC<Props> = (props) => {
   } = props;
 
   return(
-    <article className='h-full overflow-y-auto px-2 py-2'>
+    <article className='h-full overflow-y-scroll px-4 py-2'>
       {year && month && day && hour &&
-        <h1 className='text-xl'>
+        <h1 className='text-xl mb-2'>
           {year}年{month}月{day}日 {hour}:00の新着
         </h1>
       }
