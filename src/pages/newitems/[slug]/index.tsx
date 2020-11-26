8e81b77aad0a821ae83fbe2ccd8dc0da33beaacd
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-11-26 11:01:31 +0900

    refactor: 💡 newitems staticpropsfunctions move to newspages lib
    create lib/newpages. newitems staticprops functions move to lib/newpages

--- src/pages/newitems/[slug]/index.tsx
+++ src/pages/newitems/[slug]/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-import NewsCardsField, { CardType } from 'components/field/News';
-import MemberNamesArea from 'components/standalone/MemberNamesArea';
+import { CardType } from 'components/field/News';
 import LoadingField from 'components/field/Loading';
 import NewsPage from 'components/page/NewsPage';
-import { getNewsStreamerList } from 'lib/Constructions';
-import { getSchedulesBeforeData } from 'lib/firebase';
-import { VideoScheduleToNews } from 'lib/Converter';
-import { getJstTime, getUtc } from 'lib/DateFunctions';
+
+import { fetchData } from 'lib/NewsPages';
 
 import {
   GetStaticPaths,
@@ -36,31 +33,14 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const { params } = context;
   const slug = params.slug as string;
 
-  const match = getNewsStreamerList().find(x => x.slug === slug);
-  const quantity = 10; // 取得個数
-  const updateTime = getUtc(new Date());
-  updateTime.setHours(0, 0, 0, 0); // GMT 0:00 -> JST 9:00
-  const data = await getSchedulesBeforeData(VideoScheduleToNews)(match.id, updateTime, quantity);
-
-  // revalidateは現在から翌日の9:00までの差分
-  const now = getUtc(new Date());
-  const revalidateTime = new Date(updateTime);
-  revalidateTime.setDate(revalidateTime.getDate() + 1);
-  const revalidateSec = Math.floor((revalidateTime.getTime() - now.getTime()) / 1000)
-
-  // いつの更新分であるかを示す(endDateを日本時間にする)
-  const jstUpdateTime = getJstTime(updateTime);
+  const item = await fetchData(slug);
 
   return {
     props: {
+      ...item,
       slug,
-      data,
-      year: jstUpdateTime.year,
-      month: jstUpdateTime.month,
-      day: jstUpdateTime.day,
-      hour: 9,
     },
-    revalidate: revalidateSec
+    revalidate: item.secForNextUpdate
   }
 }
 
--- src/pages/newitems/index.tsx
+++ src/pages/newitems/index.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 
 import NewsPage from 'components/page/NewsPage';
-import MemberNamesArea from 'components/standalone/MemberNamesArea';
+import LoadingField from 'components/field/Loading';
+
+import { fetchData } from 'lib/NewsPages';
 
 interface OwnProps {
   year?: number,
@@ -14,42 +16,23 @@ interface OwnProps {
 
 type Props = OwnProps;
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { params } = context;
-
-  // revalidateは翌日の9:00まで
-  const now = new Date();
-  const next9H = new Date();
-  next9H.setDate(next9H.getDate() + 1);
-  next9H.setHours(0, 0, 0, 0);
-  const s = Math.floor((next9H.getTime() - now.getTime()) / 1000);
-
-  const jtNow = new Date();
-  jtNow.setHours(jtNow.getHours() + 9);
-
-  const year = jtNow.getFullYear();
-  const month = jtNow.getMonth() + 1;
-  const day = jtNow.getDate();
-  
-  const revalidateTime = s;
+export const getStaticProps: GetStaticProps = async () => {
+  const item = await fetchData();
 
   return {
     props: {
-      year,
-      month,
-      day,
-      hour: 9
+      ...item
     },
-    revalidate: revalidateTime
+    revalidate: item.secForNextUpdate
   }
 }
 
 const NewItemsPage: React.FC<Props> = (props) => {
-  const {
-    year,
-    month,
-    day,
-  } = props;
+  if(!props.year) {
+    return (
+      <LoadingField />
+    )
+  }
   return(
     <NewsPage {...props}/>
   )
