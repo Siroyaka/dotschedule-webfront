0e43df067df58fc3ccae16bce5b87254e9fe4582
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-11-26 09:39:57 +0900

    style: 💄 next and prev icon change and header padding align

--- src/components/standalone/Header.tsx
+++ src/components/standalone/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC<Props> = (props) => {
   }, [router])
 
   return(
-    <header className='w-screen flex fixed top-0 h-12 bg-white border-b px-4 py-1 flex flex-row items-center z-50 justify-between'>
+    <header className='w-screen flex fixed top-0 h-12 bg-white border-b px-2 py-1 flex flex-row items-center z-50 justify-between'>
       <div className={'flex flex-row items-center'}>
         <MenuButton classes={{button: 'hover:bg-blue-100 active:bg-blue-200'}} onClick={onMenuClick} />
         <h6 className='font-sans text-2xl text-black ml-2'>{title}</h6>
