2abbd862951ec2cadd0afbf16f5e3aac869cdbf3
siroyaka <29934865+Siroyaka@users.noreply.github.com>
2023-02-09 14:42:17 +0900

    feat: 🎸 add app directory

---
+++ app/streamingschedule/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Page: React.FC<Props> = (props) => {
+    return (
+        <React.Fragment>
+            <h1>Streaming Schedule</h1>
+        </React.Fragment>
+    )
+}
+
+export default Page
