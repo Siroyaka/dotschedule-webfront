b85c81471fa7b927bc26691af54583afbaa6c335
siroyaka <29934865+Siroyaka@users.noreply.github.com>
2023-03-15 12:49:16 +0900

    fix: 🐛 いらないコメントの削除

--- components/standalone/BottomNavigations.tsx
+++ components/standalone/BottomNavigations.tsx
@@ -71,38 +71,7 @@ const LinkTab: React.FC<NavItem & {children?: React.ReactNode}> = (props) => {
     )
 }
 
-const TabItems: React.FC<{ href?: string, children?: React.ReactNode }> = (props) => {
-    const {
-        href,
-        children,
-    } = props;
-
-    const isLink = href !== undefined;
-
-    const linkOption = isLink ? 'hover:bg-blue-200 active:bg-blue-300  duration-150 ease-in transform transition-all' : 'bg-blue-500';
-    return (
-        <React.Fragment>
-            <div className={`relative w-full h-full rounded text-xs text-center px-1 py-1 ${linkOption}`}>
-                {isLink &&
-                    <Link href={{
-                        pathname: href
-                    }}
-                        draggable={false}
-                        prefetch={false}
-                        className='absolute top-0 left-0 h-full w-full'
-                    >
-                    </Link>
-                }
-                <div className={`${isLink ? 'text-black' : 'text-white'} text-xs flex flex-col items-center`}>
-                    {children}
-                </div>
-            </div>
-        </React.Fragment>
-    )
-}
-
 const BottomNavigations2: React.FC<OwnProps> = ({ items }) => {
-    console.log(items.length);
     return (
         <Bottoms>
             <ul className='flex h-full'>
@@ -115,49 +84,6 @@ const BottomNavigations2: React.FC<OwnProps> = ({ items }) => {
                     </li>
                 ))}
             </ul>
-        </Bottoms>
-    );
-}
-
-const BottomNavigations: React.FC = () => {
-    const now = getJTCNow();
-
-    const splitPath = usePathname()?.split("/") ?? "";
-    const routePath = splitPath[2];
-    const year = splitPath.length < 4 ? now.getFullYear() : parseInt(splitPath[3]);
-    const month = splitPath.length < 5 ? now.getMonth() + 1 : parseInt(splitPath[4]);
-    const linkChild = `/${year}/${month}`;
-    const todayPath = `/streaming/day/${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`;
-
-    return (
-        <Bottoms>
-            <ul className='flex'>
-                <li className="flex-1">
-                    <TabItems href={todayPath}>
-                        <TodaySvg className='w-6' />
-                        <span>Today</span>
-                    </TabItems>
-                </li>
-                <li className="flex-1">
-                    <TabItems href={routePath !== 'calendar' ? `/streaming/calendar${linkChild}` : undefined}>
-                        <CalendarSvg className='w-6' />
-                        <span>Calendar</span>
-                    </TabItems>
-                </li>
-                <li className="flex-1">
-                    <TabItems href={routePath !== 'monthlist' ? `/streaming/monthlist${linkChild}` : undefined}>
-                        <ListSvg className='w-6' />
-                        <span>List</span>
-                    </TabItems>
-                </li>
-                <li className="flex-1">
-                    <TabItems href={routePath !== 'search' ? '/streaming/search' : undefined}>
-                        <SearchIconSvg className='w-6' />
-                        <span>Search</span>
-                    </TabItems>
-                </li>
-            </ul>
-
         </Bottoms>
     );
 }
