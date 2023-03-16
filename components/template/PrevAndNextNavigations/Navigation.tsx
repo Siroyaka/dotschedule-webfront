cc28475402ad4f87902af13ff46ef633f2c93f42
siroyaka <29934865+Siroyaka@users.noreply.github.com>
2023-03-16 11:55:14 +0900

    refactor: 💡 検索のページ送りの数値の配置の固定化など

--- components/template/PrevAndNextNavigations/Navigation.tsx
+++ components/template/PrevAndNextNavigations/Navigation.tsx
@@ -12,9 +12,11 @@ type Props = OwnProps;
 
 const Navigation: React.FC<Props> = (props) => {
   const { disabled, href, children } = props;
-  const linkOption = disabled ? 'text-gray-300' : 'text-black active:bg-blue-200 hover:bg-blue-100 duration-200 ease-in transition';
   return(
-    <div className={`relative rounded-full ${linkOption}`}>
+    <div className={`
+      relative rounded-full
+      ${disabled ? 'text-gray-300' : 'text-black active:bg-blue-200 hover:bg-blue-100 duration-200 ease-in transition'}
+    `}>
       {!disabled && 
         <Link
           className='absolute h-full w-full top-0 left-0'
