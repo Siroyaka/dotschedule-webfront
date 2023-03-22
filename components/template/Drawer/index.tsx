60b429bde3f01f322c75356e73770b02fb749d43
siroyaka <29934865+Siroyaka@users.noreply.github.com>
2023-03-22 15:26:56 +0900

    style: 💄 ページヘッダーのレイアウトを前のものにもどした。

--- components/template/Drawer/index.tsx
+++ components/template/Drawer/index.tsx
@@ -21,8 +21,8 @@ const Drawer: React.FC<Props> = (props) => {
         open &&
         <div id='drawer-out-field' onClick={onClose} className='h-full absolute z-20 top-0 bg-gray-300 bg-opacity-25 w-full' />
       }
-      <aside className={clsx(className, 'bg-white transform duration-300 ease-in-out h-full fixed z-40 top-0 right-0 transition-all flex flex-col w-64 border-r border-gray-200 px-2', `${open ? 'translate-x-0' : 'translate-x-full'}`)}>
-        <div id='drawerHeader' className='h-12 bg-white border-b w-full flex flex-row justify-end py-1'>
+      <aside className={clsx(className, 'bg-white transform duration-300 ease-in-out h-full fixed z-40 top-0 left-0 transition-all flex flex-col w-64 border-r border-gray-200 px-2', `${open ? 'translate-x-0' : '-translate-x-full'}`)}>
+        <div id='drawerHeader' className='h-12 bg-white border-b w-full flex flex-row justify-start py-1'>
           <button aria-label='close-left-drawer' className='py-2 px-2 focus:outline-none transition duration-200 rounded-full hover:bg-blue-100 active:bg-blue-200' style={{transition: 'all .15 ease'}} onClick={onClose}>
             <CloseSvg className='w-6'/>
           </button>
